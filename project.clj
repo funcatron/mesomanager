6692abe61c5fa7e61bbc5538a10282d88b71bff0
David Pollak <feeder.of.the.bears@gmail.com>
2016-10-14 16:56:14 -0700

    Added the javarx mesos library

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+                 [com.mesosphere.mesos.rx.java/mesos-rxjava-client "0.1.0"]])
--- src/mesosmanager/core.clj
+++ src/mesosmanager/core.clj
@@ -1,6 +1,14 @@
-(ns mesosmanager.core)
+(ns mesosmanager.core
+  (:import (com.mesosphere.mesos.rx.java MesosClientBuilder)
+           (java.net URI)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(->
+  (MesosClientBuilder/newBuilder)
+  (.mesosUri (URI. "http://localhost:5050"))
+  .build
+  )
